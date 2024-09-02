fbe6cd8a4ed01044e1bef8d12199e9dc02444b57
Dylan Jenkins <dylanljenkins@gmail.com>
2024-09-02 21:10:09 +1000

    Colour

--- theme.ts
+++ theme.ts
@@ -1,8 +1,20 @@
 "use client";
 import { createTheme } from '@mui/material/styles';
+import {yellow,} from "@mui/material/colors";
 const lightTheme = createTheme();
 
-const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: yellow[200]
+    },
+    background: {
+      default: "#020617",
+      paper: "#0f172a"
+    }
+  }
+});
 const theme = {
   light: lightTheme,
   dark: darkTheme
