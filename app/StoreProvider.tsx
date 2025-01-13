dd7e783f127b14f2c5c722e73c0138f110f47f22
Dylan Jenkins <dylanljenkins@gmail.com>
2025-01-13 19:27:22 +1100

    Added redux persist

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -2,14 +2,27 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import {AppStore, makeStore} from "@/lib/store/util/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import {Persistor, persistStore} from "redux-persist";
 
 export default function StoreProvider({children,}: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore>()
+  const persistorRef = useRef<Persistor>()
 
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>
+  if (!persistorRef.current) {
+    persistorRef.current = persistStore(storeRef.current)
+  }
+
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistorRef.current}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
--- lib/store/util/store.ts
+++ lib/store/util/store.ts
@@ -1,13 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore } from '@reduxjs/toolkit'
 import {agentReducer} from "@/lib/store/agentStore";
 import {enemyReducer} from "@/lib/store/enemyStore";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+const rootReducer = combineReducers({
+    agent: agentReducer,
+    enemy: enemyReducer
+})
+
+// Create a persist configuration
+const persistConfig = {
+    key: 'persist',
+    version: 1,
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const makeStore = () => {
     return configureStore({
-        reducer: {
-            agent: agentReducer,
-            enemy: enemyReducer
-        },
+        reducer: persistedReducer
     })
 }
 
